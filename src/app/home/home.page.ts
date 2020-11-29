8cad930d0821d81c4da4cbb4dfeb534b116dcf2c
Josh McKinney <jmckinney.dev@gmail.com>
2020-11-29 01:53:41 -0800

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public events:any;
+  
+  constructor(
+    private http : HttpClient) {
+      this.getData();
+    };
+
+  getData(){
+    let url = 'https://api.apify.com/v2/actor-tasks/dbopD6AOZdk6xD0oO/runs/last/dataset/items?token=3j2tbgAmEpLpyWAr5Jv6rhqh4&offset=1'
+    let data: Observable<any> = this.http.get(url);
+    data.subscribe(result => {
+      this.events = result;
+      console.log(result);
+    })
+  };
 
 }
